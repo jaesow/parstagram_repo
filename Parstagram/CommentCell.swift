d6f83850dd59993d6028b9549169d0c2b9b18dfd
Jenaba Sow <jenabasow@gmail.com>
2021-04-19 13:47:13 -0400

    parstagram app + read me + unit5 and 6 gifs

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Jenaba Sow
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
